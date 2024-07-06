62aea4f87c585fce8205a50c87b8f523a1d754bd
nerdprojects <112236216+nerdprojects@users.noreply.github.com>
2024-07-07 01:18:18 +0200

    Improve Basic Loader

--- loader/write-bas.py
+++ loader/write-bas.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import serial
 import sys
@@ -10,26 +10,31 @@ import platform
 # LOAD"COM0:(68N13)"
 
 if len(sys.argv) < 3:
-  print "provide serial device and file name"
+  print('provide serial device and file name')
   sys.exit(-1)
 
 serial_device = sys.argv[1]
 file_name = sys.argv[2]
-serial_handle = serial.Serial(serial_device, 4800, bytesize=8, parity=serial.PARITY_NONE, stopbits=1, rtscts=1)
-file_handle = open(file_name,"rb")
+serial_handle = serial.Serial(serial_device, 4800, bytesize = 8, parity = serial.PARITY_NONE, stopbits = 1, rtscts = 1)
+file_handle = open(file_name, 'rb')
 input_byte = file_handle.read(1)
-while input_byte != "":
+while input_byte:
   serial_handle.write(input_byte)
-  # for the mac, i needed to add a delay after every line
-  # i think on the mac, there might be something wrong with the serial port configuration or the FTDI driver,
-  # not sure...
-  if platform.system() == 'Darwin':
-    if(input_byte == '\n'):
-      time.sleep(0.5)
-  sys.stdout.write(input_byte)
+  # the HX-20 Basic seems to need some time to interpret or storing the data we send,
+  # which can result in a "BO Error" if we send it too fast, therefore we delay here
+  time.sleep(0.01)
+
+  character = '.'
+  try:
+    character = input_byte.decode('ascii')
+  except UnicodeDecodeError:
+    pass
+
+  sys.stdout.write(character)
   sys.stdout.flush()
   input_byte = file_handle.read(1)
 
+print('done')
 serial_handle.flush()
 file_handle.close()
 serial_handle.close()
